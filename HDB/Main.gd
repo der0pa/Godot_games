ff4e32be6abfc15d87a8a515abe1b8829fc3f1ee
der0pa <dsv2xm@gmail.com>
2020-10-25 12:43:26 -0400

    add Mob.tscn

---
+++ HDB/Main.gd
@@ -0,0 +1,18 @@
+extends Node
+
+export (PackedScene) var Mob
+var score
+
+func _ready():
+	randomize()
+
+func _on_StartTimer_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_ScoreTimer_timeout() -> void:
+	pass # Replace with function body.
+
+
+func _on_MobTimer_timeout() -> void:
+	pass # Replace with function body.
