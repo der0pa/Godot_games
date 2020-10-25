d427126beda001c26f3c3fc576ce6309a88ca05f
der0pa <dsv2xm@gmail.com>
2020-10-25 11:00:27 -0400

    broken

--- HDB/Player.gd
+++ HDB/Player.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
-
+signal hit
 # Declare member variables here. Examples:
+var score = 0
 # var a: int = 2
 # var b: String = "text"
 export var speed = 400
@@ -49,3 +50,48 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
+
+
+func _on_Player_body_entered(body: Node) -> void:
+	hide()  # Player disappears after being hit.
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	
+	
+func _on_MobTimer_timeout():
+	# Choose a random location on Path2D.
+	$MobPath/MobSpawnLocation.offset = randi()
+	# Create a Mob instance and add it to the scene.
+	var mob = Mob.instance()
+	add_child(mob)
+	# Set the mob's direction perpendicular to the path direction.
+	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+	# Set the mob's position to a random location.
+	mob.position = $MobPath/MobSpawnLocation.position
+	# Add some randomness to the direction.
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	# Set the velocity (speed & direction).
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
